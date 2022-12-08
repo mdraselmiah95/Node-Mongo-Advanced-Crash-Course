86b7c1bd7483cd925cb628d334e8cefa126385b7
mdraselmiah95 <russelroy15@gmail.com>
2022-12-09 01:38:08 +0600

    Leaning Eror

--- Javascript-For-Backend-Development/Errors.js
+++ Javascript-For-Backend-Development/Errors.js
@@ -1,20 +1,26 @@
 async function getData() {
   try {
-    undefined.find();
-  } catch (error) {}
+    // undefined.find();
+    const emailError = new Error("Email Error ⛑️");
+    throw emailError;
+  } catch (error) {
+    errorHandler(error);
+  }
 }
 
 getData();
 
 function errorHandler(error) {
   const { name, message, stack } = error;
+  console.log(name, message);
 
-  logger.error({
-    name,
-    message,
-    stack,
-  });
-  console.log("Internal Server Error.");
+  //   logger.error({
+  //     name,
+  //     message,
+  //     stack,
+  //   });
+  //   console.log("Internal Server Error.");
 }
 
 console.log("DONE");
+//gdgd   the fun
