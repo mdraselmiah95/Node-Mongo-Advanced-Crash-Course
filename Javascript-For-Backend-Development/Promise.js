108e485378ac22a4c0e2f3c373e349c39c7181fa
mdraselmiah95 <russelroy15@gmail.com>
2022-12-08 23:15:52 +0600

    Leaning Promise

--- Javascript-For-Backend-Development/Promise.js
+++ Javascript-For-Backend-Development/Promise.js
@@ -1,15 +1,27 @@
 // PENDING => RESOLVED => REJECTED
 
 const myPromise = new Promise((resolve, reject) => {
-  const user = null;
+  const user = "Rasel";
 
   if (!user) {
     reject("Something went wrong 💥");
   } else {
     setTimeout(() => {
-      resolve("Successfully Got The Data");
+      resolve({ name: "Rasel" });
     }, 2000);
   }
 });
+
+const userIds = [1, 2, 3, 4, 5, 6];
+let userData = [];
+
+for (let i = 0; i < userIds.length; i++) {
+  const userId = userIds[i];
+  myPromise.then((user) => {
+    userData.push(user);
+  });
+}
+
+console.log(userData);
 
 myPromise.then((res) => console.log(res)).catch((err) => console.log(err));
