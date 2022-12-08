9adcaf176075c2bc2ab0d61d7cd07b6a2ea18a25
mdraselmiah95 <russelroy15@gmail.com>
2022-12-09 01:53:50 +0600

    Leaning Error

--- Javascript-For-Backend-Development/errorHandler.js
+++ Javascript-For-Backend-Development/errorHandler.js
@@ -10,18 +10,23 @@ function errorHandler(error) {
   //   console.log("Internal Server Error.");
 }
 
+// module.exports.funk = function funk() {
+//   console.log("TWO");
+// };
 function funk() {
   console.log("TWO");
 }
 
-//module.exports = errorHandler;
+// module.exports = errorHandler;
 
-// module.exports.errorHandler = errorHandler;
-// module.exports.funk = funk;
+module.exports.errorHandler = errorHandler;
+module.exports.funk = funk;
 
-module.exports = {
-  errorHandler,
-  funk,
-};
+// module.exports = {
+//   errorHandler,
+//   funk,
+// };
 
 // console.log(module);
+
+// export default funk;
