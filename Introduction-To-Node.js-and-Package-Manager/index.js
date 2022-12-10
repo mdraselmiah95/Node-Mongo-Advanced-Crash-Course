fd7607904d0cd39eb2cd6db1625eee636122b6f9
mdraselmiah95 <russelroy15@gmail.com>
2022-12-10 20:54:14 +0600

    Learning NodeJS

--- Introduction-To-Node.js-and-Package-Manager/index.js
+++ Introduction-To-Node.js-and-Package-Manager/index.js
@@ -1,5 +1,20 @@
 const { name, add } = require("./other");
+const http = require("http");
+const _ = require("underscore");
 
 console.log(add(10, 5));
 
-console.log(name);
+const server = http.createServer((req, res) => {
+  //   console.log("server is running");
+});
+
+// server.listen(5000);
+
+var data = [
+  { name: "Rasel", age: 38 },
+  { name: "Shakib", age: 39 },
+  { name: "Tamim", age: 43 },
+];
+
+const result = _.pluck(data, "name");
+console.log(result);
