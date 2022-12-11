90c508c4e5cec5b83b1099358c4a10e4df7f3885
mdraselmiah95 <russelroy15@gmail.com>
2022-12-12 00:41:28 +0600

    Learning NodeJS

--- Introduction-To-Node.js-and-Package-Manager/index.js
+++ Introduction-To-Node.js-and-Package-Manager/index.js
@@ -19,7 +19,29 @@ const other = require("./other");
 // const result = _.pluck(data, "name");
 // console.log(result);
 
-const res1 = other.add(10, 15);
-const res2 = other.subtract(35, 15);
-console.log(res1);
-console.log(res2);
+// const res1 = other.add(10, 15);
+// const res2 = other.subtract(35, 15);
+// console.log(res1);
+// console.log(res2);
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<p>This is Home page.</p>`);
+    res.end();
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<p>This is Contact page.</p>`);
+    res.end();
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`<p>This is About page.</p>`);
+    res.end();
+  }
+});
+
+const PORT = 5000;
+server.listen(PORT);
+console.log(`Server is running at ${PORT}`);
