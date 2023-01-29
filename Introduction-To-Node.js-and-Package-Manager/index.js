ee41d86dcf3373df24c2e7599595f4451e6420ad
mdraselmiah95 <russelroy15@gmail.com>
2023-01-29 22:14:06 +0600

    MVC Model–view–controller

--- Introduction-To-Node.js-and-Package-Manager/index.js
+++ Introduction-To-Node.js-and-Package-Manager/index.js
@@ -5,10 +5,10 @@ const other = require("./other");
 // console.log(add(10, 5));
 
 // const server = http.createServer((req, res) => {
-//   //   console.log("server is running");
+//      console.log("server is running");
 // });
 
-// // server.listen(5000);
+//  server.listen(5000);
 
 // var data = [
 //   { name: "Rasel", age: 38 },
