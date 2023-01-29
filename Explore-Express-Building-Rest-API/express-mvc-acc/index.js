7d3a4b44a15027907b4988f3c14f88b58bb41bc2
mdraselmiah95 <russelroy15@gmail.com>
2023-01-30 00:07:53 +0600

    MVC Model–view–controller

--- Explore-Express-Building-Rest-API/express-mvc-acc/index.js
+++ Explore-Express-Building-Rest-API/express-mvc-acc/index.js
@@ -8,14 +8,14 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const nodemailer = require("nodemailer");
 const dbConnect = require("./utils/dbConnect");
-const toolsRouters = require("./routes/tools.route");
+const toolsRouters = require("./routes/v1/tools.route");
 
 app.use(cors());
 app.use(express.json());
 
 dbConnect(); // Connect to the DataBase
 
-app.use("/tools", toolsRouters);
+app.use("/api/v1/tools", toolsRouters);
 
 async function run() {
   try {
---
+++ Explore-Express-Building-Rest-API/express-mvc-acc/routes/v1/tools.route.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.send("Tools Found");
+// });
+
+// router.post("/", (req, res) => {
+//   res.send("Tool Added");
+// }); 17.06 lowest
+
+router
+  .route("/")
+  /**
+   * @api {get} /tools All tools
+   * @apiDescription Get all the tools
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get((req, res) => {
+    res.send("Tools Found");
+  })
+  /**
+   * @api {post} /tools save a tool
+   * @apiDescription Get all the tools
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .post((req, res) => {
+    res.send("Tool Added");
+  });
+
+module.exports = router;
