793a6b2b303bd1582087a2136737999e78cd1518
mdraselmiah95 <russelroy15@gmail.com>
2023-02-02 01:02:06 +0600

    CRUD Operation

--- Explore-Express-Building-Rest-API/express-mvc-acc/index.js
+++ Explore-Express-Building-Rest-API/express-mvc-acc/index.js
@@ -13,6 +13,7 @@ const viewCount = require("./middleware/viewCount");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 // Middleware
 // app.use(viewCount);
@@ -203,6 +204,7 @@ run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+  res.sendFile(__dirname + "/public/test.html");
 });
 
 app.all("*", (req, res) => {
