c489f9b776ab20f6651a8c65d360519c64326afc
mdraselmiah95 <russelroy15@gmail.com>
2023-02-08 22:13:22 +0600

    Creating Brand Model

--- Introduction-to-Mongoose-and-Implement/inventory-management/models/Brand.js
+++ Introduction-to-Mongoose-and-Implement/inventory-management/models/Brand.js
@@ -1,11 +1,53 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
 
-const brandSchema = mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: [true, "Please Provide a Brand Name"],
-    maxLength: 100,
+const brandSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: [true, "Please Provide a Brand Name"],
+      maxLength: 100,
+      lowercase: true,
+    },
+    description: String,
+    email: {
+      type: String,
+      lowercase: true,
+      validator: [validator.isEmail, "Please Provide a Valid Email"],
+    },
+    website: {
+      type: String,
+      validator: [validator.isURL, "Please Provide a Valid URL"],
+    },
+    location: String,
+    products: [
+      {
+        type: ObjectId,
+        ref: "Product",
+      },
+    ],
+    supplier: [
+      {
+        name: String,
+        contactNumber: String,
+        id: {
+          type: ObjectId,
+          ref: "Supplier",
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "active",
+    },
   },
-});
+  { timestamps: true }
+);
+
+const Brand = mongoose.model("Brand", brandSchema);
+
+module.exports = Brand;
