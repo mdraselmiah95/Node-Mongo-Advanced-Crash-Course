e0a3d17143b3aeb16d191e9c8c224f9863be6a64
mdraselmiah95 <russelroy15@gmail.com>
2023-02-15 16:45:49 +0600

    Uploading file clint to server with multer

--- Introduction-to-Mongoose-and-Implement/inventory-management/middleware/uploader.js
+++ Introduction-to-Mongoose-and-Implement/inventory-management/middleware/uploader.js
@@ -3,16 +3,16 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: "images/",
-  filename: (req, file, cd) => {
+  filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "-" + file.originalname);
   },
 });
 
-const uploader = multer.MulterError({
+const uploader = multer({
   storage,
   fileFilter: (req, file, cb) => {
-    const supportedImage = "/png|jpg/";
+    const supportedImage = /png|jpg|webp/;
     const extension = path.extname(file.originalname);
 
     if (supportedImage.test(extension)) {
@@ -22,7 +22,7 @@ const uploader = multer.MulterError({
     }
   },
   limits: {
-    FileSize: 5000000,
+    fileSize: 5000000,
   },
 });
 
