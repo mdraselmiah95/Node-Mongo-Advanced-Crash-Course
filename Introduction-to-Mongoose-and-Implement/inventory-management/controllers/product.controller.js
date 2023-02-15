4687c27c1385dd98f6dca1b0d4159203a5d3a2c1
mdraselmiah95 <russelroy15@gmail.com>
2023-02-15 15:41:44 +0600

    Uploading file clint to server with multer

--- Introduction-to-Mongoose-and-Implement/inventory-management/controllers/product.controller.js
+++ Introduction-to-Mongoose-and-Implement/inventory-management/controllers/product.controller.js
@@ -163,3 +163,9 @@ exports.bulkDeleteProduct = async (req, res, next) => {
     });
   }
 };
+
+exports.fileUpload = async (req, res) => {
+  try {
+    res.status(200).json(req.file);
+  } catch (error) {}
+};
