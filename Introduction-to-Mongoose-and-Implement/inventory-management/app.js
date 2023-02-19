0c1dc99daf9f39702ff0134f0ac78db7329f111e
mdraselmiah95 <russelroy15@gmail.com>
2023-02-19 16:10:04 +0600

    Category Model added

--- Introduction-to-Mongoose-and-Implement/inventory-management/app.js
+++ Introduction-to-Mongoose-and-Implement/inventory-management/app.js
@@ -10,6 +10,7 @@ app.use(cors());
 // Route
 const productRoute = require("./routes/product.route");
 const brandRoute = require("./routes/brand.route");
+const categoryRoute = require("./routes/category.route");
 // const storeRoute = require("./routes/store.route");
 // const supplierRoute = require("./routes/supplier.route");
 
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/brand", brandRoute);
+app.use("/api/v1/category", categoryRoute);
 // app.use("/api/v1/store", storeRoute);
 // app.use("/api/v1/supplier", supplierRoute);
 
