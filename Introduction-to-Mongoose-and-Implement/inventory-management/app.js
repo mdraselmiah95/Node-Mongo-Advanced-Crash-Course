c942f0b585d15748ab2ed8f614cc21506ecea99f
mdraselmiah95 <russelroy15@gmail.com>
2023-02-19 16:03:16 +0600

    Store Model added

--- Introduction-to-Mongoose-and-Implement/inventory-management/app.js
+++ Introduction-to-Mongoose-and-Implement/inventory-management/app.js
@@ -10,6 +10,7 @@ app.use(cors());
 // Route
 const productRoute = require("./routes/product.route");
 const brandRoute = require("./routes/brand.route");
+// const storeRoute = require("./routes/store.route");
 // const supplierRoute = require("./routes/supplier.route");
 
 //TODO: SCHEMA => MODEL => QUERY
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/brand", brandRoute);
+// app.use("/api/v1/store", storeRoute);
 // app.use("/api/v1/supplier", supplierRoute);
 
 module.exports = app;
