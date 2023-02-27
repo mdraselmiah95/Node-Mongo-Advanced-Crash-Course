197bd2f8e7337d0f161158f85113c1425ec54cc8
mdraselmiah95 <russelroy15@gmail.com>
2023-02-28 01:12:32 +0600

    Learning Aggression

--- Introduction-to-Mongoose-and-Implement/inventory-management/services/stock.service.js
+++ Introduction-to-Mongoose-and-Implement/inventory-management/services/stock.service.js
@@ -26,6 +26,24 @@ exports.getStockByIdService = async (id) => {
   const stock = await Stock.aggregate([
     //stage1
     { $match: { _id: ObjectId(id) } },
+    {
+      $project: {
+        name: 1,
+        category: 1,
+        price: 1,
+        productId: 1,
+        quantity: 1,
+        "brand.name": 1,
+      },
+    },
+    {
+      $lookup: {
+        from: "brands",
+        localField: "brand.name",
+        foreignField: "name",
+        as: "brandDetails",
+      },
+    },
   ]);
 
   return stock;
