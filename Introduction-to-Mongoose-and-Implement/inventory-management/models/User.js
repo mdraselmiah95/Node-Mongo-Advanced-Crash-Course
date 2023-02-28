7c913e504392ff7c895d3e908682c13048b51dc4
mdraselmiah95 <russelroy15@gmail.com>
2023-03-01 00:23:47 +0600

    User added

---
+++ Introduction-to-Mongoose-and-Implement/inventory-management/models/User.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const crypto = require("crypto");
+
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Provide a valid Email"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: [true, "Email address is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: (value) =>
+          validator.isStrongPassword(value, {
+            minLength: 6,
+            minLowercase: 3,
+            minNumbers: 1,
+            minUppercase: 1,
+            minSymbols: 1,
+          }),
+        message: "Password {VALUE} is not strong enough.",
+      },
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (value) {
+          return value === this.password;
+        },
+        message: "Passwords don't match!",
+      },
+    },
+
+    role: {
+      type: String,
+      enum: ["buyer", "store-manager", "admin"],
+      default: "buyer",
+    },
+
+    firstName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [100, "Name is too large"],
+    },
+    contactNumber: {
+      type: String,
+      validate: [
+        validator.isMobilePhone,
+        "Please provide a valid contact number",
+      ],
+    },
+
+    shippingAddress: String,
+
+    imageURL: {
+      type: String,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+    status: {
+      type: String,
+      default: "inactive",
+      enum: ["active", "inactive", "blocked"],
+    },
+
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
