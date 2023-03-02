2e94f0cf893a464a18cd71f0cf54d02df9f351cc
mdraselmiah95 <russelroy15@gmail.com>
2023-03-02 22:37:56 +0600

    REST API 101 create player

--- REST-API-101/src/server.js
+++ REST-API-101/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const shortid = require("shortid");
 
 const app = express();
 
@@ -13,14 +14,22 @@ app.use(express.json());
  *
  * CRUD ==> Create, Read, Update, Delete
  *
- * GET -/    -find all the players
- * POST -/   -create a new and save into DB
+ * GET -/       -find all the players
+ * POST -/      -create a new and save into DB
  * GET -/:id    -find a single player by id
  * PUT -/:id    -update or create a player
  * PATCH -/:id  -update player
  * DELETE -/:id -delete a player from DB
  *
  */
+
+app.post("/", (req, res) => {
+  const player = {
+    ...req.body,
+    id: shortid.generate(),
+  };
+  res.status(201).json(player);
+});
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
