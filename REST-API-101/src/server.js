a944186e17f2843ea73bf53a6458ff7401ca5d82
mdraselmiah95 <russelroy15@gmail.com>
2023-03-03 00:19:04 +0600

    REST API 101 create and get player

--- REST-API-101/src/server.js
+++ REST-API-101/src/server.js
@@ -26,6 +26,22 @@ app.use(express.json());
  *
  */
 
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+  const player = players.find((item) => item.id === id);
+
+  if (!player) {
+    return res.status(404).json({ message: "Player Not Found" });
+  }
+
+  const newPlayers = players.filter((item) => item.id !== id);
+  await fs.writeFile(dbLocation, JSON.stringify(newPlayers));
+  res.status(203).send();
+});
+
 app.put("/:id", async (req, res) => {
   const id = req.params.id;
 
@@ -40,12 +56,12 @@ app.put("/:id", async (req, res) => {
     };
     players.push(player);
   } else {
-    player = {
-      id: player.id,
-      ...req.body,
-    };
+    player.name = req.body.name;
+    player.country = req.body.country;
+    player.rank = req.body.rank;
   }
   await fs.writeFile(dbLocation, JSON.stringify(players));
+  res.status(200).json(player);
 });
 
 app.patch("/:id", async (req, res) => {
